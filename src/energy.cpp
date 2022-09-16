1aaf85dca8f88b8f4bc25dd1b0e25b8d49c5bf97
eslamsalahelsheikh@gmail.com <eslamsalahelsheikh@gmail.com>
2022-09-16 23:48:05 +0200

    using local kinetic energy to calculate temp

--- src/energy.cpp
+++ src/energy.cpp
@@ -91,7 +91,7 @@ void Energy::update_gupta(Atoms &atoms, NeighborList &neighbor_list,double cutof
     potential_energy_ = MPI::allreduce(per_atom_potential_energy, MPI_SUM, MPI_COMM_WORLD);
     kinetic_energy_ = MPI::allreduce(per_atom_kinetic_energy, MPI_SUM, MPI_COMM_WORLD);
     total_energy_ = total_energy();
-    temperature_ = calculate_temperature(atoms, false);
+    temperature_ = calculate_temperature(atoms, true);
     std::cout << "total energy: " << total_energy_    << " temperature: " << temperature_ << " potential energy: " << potential_energy_ << std::endl;
 }
 
