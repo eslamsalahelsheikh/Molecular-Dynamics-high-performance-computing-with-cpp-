b333f6bd0a5269831b174e890fa39654c62c3cd6
eslamsalahelsheikh@gmail.com <eslamsalahelsheikh@gmail.com>
2022-09-20 23:59:29 +0200

    Fix Brendsen test

--- tests/test_berendsen_thermostat.cpp
+++ tests/test_berendsen_thermostat.cpp
@@ -36,61 +36,55 @@ TEST(BerendsenThermostatTest, SingleAtomExponentialDecay) {
     double epsilon = 1.0;
     double sigma = 1.0;
 
-    double t_tot = 100; // total simulation time
     double dt = 0.01; // timestep
     double tau = 1000.*dt; // big enough relaxation time for the approximation e^-dt/tau=-dt/tau to hold
-    int steps = t_tot/dt; // number of simulation steps
-    double T_target = 2.0;
+    int steps = 10000; // number of simulation steps
+    double T_target = 0.1; // target temperature
 
     Atoms atoms(lattice(1, 1, 1, 1));
     atoms.velocities.setOnes();
     Energy energy(atoms, epsilon, sigma, mass);
 
-    double T_init = energy.get_temperature();
-    double t = 0.0;
-    double T_curr; // current temperature
-    double T_calc; // calculated temperature with exponential decay
+    double T_init, T_curr, T_calc;
 
     for(int i=1; i<=steps; i++){ // main sim loop
-        T_curr = energy.get_temperature();
-        T_calc = T_target+(T_init-T_target)*exp(-t/tau);
-//        std::cout << T_curr << std::endl; // print current temperature
-//        std::cout << T_calc << std::endl; // print expected
         EXPECT_NEAR(T_curr, T_calc, T_calc*0.01); // check whether T_curr=T_calc+-1%
         verlet_step1(atoms, dt,1);
         energy.energy_update(atoms, epsilon, sigma);
         verlet_step2(atoms, dt,1);
+        T_init = energy.get_temperature();
         energy.berendsen_thermostat(atoms, T_target, dt, tau);
-        t += dt;
+        T_curr = energy.get_temperature();
+        T_calc = T_target+(T_init-T_target)*exp(-dt/tau);
     }
 }
 
 // equilibrate an atom cluster to T = 0.1 epsilon
 // and check whether T converges to the desired T
-//TEST(BerendsenThermostatTest, FinalConvergence) {
-//    double mass = 1.0;
-//    double epsilon = 1.0;
-//    double sigma = 1.0;
-//
-//    double t_tot = 100; // total simulation time
-//    double dt = 0.01; // timestep
-//    double tau = 10.*dt; // relaxation time
-//    int steps = t_tot/dt; // number of simulation steps
-//    double T_target = 0.1;
-//
-//    Atoms atoms(lattice(5, 5, 5, sigma*1.12)); // initialize 5x5x5 lattice
-//    Energy energy(atoms, epsilon, sigma, mass);
-//    for(int i=1; i<=steps; i++){ // main sim loop
-//        std::cout << "step " << i << std::endl; // print current step
-//        verlet_step1(atoms, dt, 1);
-//        energy.update(atoms, epsilon, sigma);
-//        verlet_step2(atoms, dt, 1);
-//        energy.berendsen_thermostat(atoms, T_target, dt, tau);
-//        if(i>steps/2){ // only check after convergence
-//            std::cout << energy.temperature(atoms) << std::endl; // print current temperature
-//            EXPECT_NEAR(energy.temperature(atoms), T_target, T_target*0.2); // check whether T=T_target+-20%
-//        }
-//    }
-//}
+TEST(BerendsenThermostatTest, FinalConvergence) {
+    double mass = 1.0;
+    double epsilon = 1.0;
+    double sigma = 1.0;
+
+    double t_tot = 100; // total simulation time
+    double dt = 0.01; // timestep
+    double tau = 10.*dt; // relaxation time
+    int steps = t_tot/dt; // number of simulation steps
+    double T_target = 0.1;
+
+    Atoms atoms(lattice(5, 5, 5, sigma*1.12)); // initialize 5x5x5 lattice
+    Energy energy(atoms, epsilon, sigma, mass);
+    for(int i=1; i<=steps; i++){ // main sim loop
+        std::cout << "step " << i << std::endl; // print current step
+        verlet_step1(atoms, dt, 1);
+        energy.energy_update(atoms, epsilon, sigma);
+        verlet_step2(atoms, dt, 1);
+        energy.berendsen_thermostat(atoms, T_target, dt, tau);
+        if(i>steps/2){ // only check after convergence
+//            std::cout << energy.get_temperature() << std::endl; // print current temperature
+            EXPECT_NEAR(energy.get_temperature(), T_target, T_target*0.2); // check whether T=T_target+-20%
+        }
+    }
+}
 
 
