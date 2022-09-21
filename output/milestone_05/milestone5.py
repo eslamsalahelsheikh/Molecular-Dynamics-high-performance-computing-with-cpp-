9bc0bcdf88c3835d1e3aa1c4709d3b02b487e98c
eslamsalahelsheikh@gmail.com <eslamsalahelsheikh@gmail.com>
2022-09-21 23:04:24 +0200

    change name of cluster size to atoms number

--- output/milestone_05/milestone5.py
+++ output/milestone_05/milestone5.py
@@ -2,13 +2,13 @@ import pandas as pd
 from matplotlib import pyplot as plt
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
-columns = ["cluster_size","simulation_time"]
-df = pd.read_csv("simulation_time_vs_cluster_size.csv", usecols=columns)
-plt.plot(df.cluster_size, df.simulation_time)
-plt.xlabel("Cluster size")
+columns = ["atoms_number","simulation_time"]
+df = pd.read_csv("simulation_time_vs_atoms_number.csv", usecols=columns)
+plt.plot(df.atoms_number, df.simulation_time)
+plt.xlabel("Atoms number")
 plt.ylabel("Simulation time (s)")
-plt.title("Simulation Time vs Cluster Size")
+plt.title("Simulation time vs Atoms number")
 plt.grid(0.1)
-plt.savefig("simulation_time_vs_cluster_size.png")
+plt.savefig("simulation_time_vs_atoms_number.png")
 plt.show()
 
